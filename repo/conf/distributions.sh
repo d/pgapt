ff0651a8bf35429c0e7b4b47dad9f541b1000ff3
Christoph Berg <myon@debian.org>
2014-05-15 08:01:12 +0200

    Add 9.4 to the release distributions

--- repo/conf/distributions.sh
+++ repo/conf/distributions.sh
@@ -11,11 +11,11 @@ for DIST in $DISTRIBUTIONS ; do
 			lenny|etch)
 				COMPONENTS="main 8.2 8.3 8.4 9.0 9.1 9.2" ;;
 			lucid)
-				COMPONENTS="main 8.3 8.4 9.0 9.1 9.2 9.3" ;;
+				COMPONENTS="main 8.3 8.4 9.0 9.1 9.2 9.3 9.4" ;;
 			trusty)
-				COMPONENTS="main 8.4 9.0 9.1 9.2 9.3" ;;
+				COMPONENTS="main 8.4 9.0 9.1 9.2 9.3 9.4" ;;
 			wheezy|squeeze|saucy|precise)
-				COMPONENTS="main 8.2 8.3 8.4 9.0 9.1 9.2 9.3" ;;
+				COMPONENTS="main 8.2 8.3 8.4 9.0 9.1 9.2 9.3 9.4" ;;
 			sid)
 				COMPONENTS="main 8.2 8.3 8.4 9.0 9.1 9.2 9.3 9.4" ;;
 			*)
