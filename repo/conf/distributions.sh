1cf76ae7c7a358df8f5be3f91a3ac4b271f8bcd2
Christoph Berg <myon@debian.org>
2014-12-17 20:17:41 +0100

    Add 8.4 for Jessie so we don't forget

--- repo/conf/distributions.sh
+++ repo/conf/distributions.sh
@@ -17,7 +17,7 @@ for DIST in $DISTRIBUTIONS ; do
 			wheezy|squeeze|saucy|precise)
 				COMPONENTS="main 8.2 8.3 8.4 9.0 9.1 9.2 9.3 9.4" ;;
 			jessie)
-				COMPONENTS="main 9.0 9.1 9.2 9.3 9.4" ;;
+				COMPONENTS="main 8.4 9.0 9.1 9.2 9.3 9.4" ;;
 			sid)
 				COMPONENTS="main 8.2 8.3 8.4 9.0 9.1 9.2 9.3 9.4 9.5" ;;
 			*)
