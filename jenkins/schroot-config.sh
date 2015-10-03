165ef9fd010dd918c5e42378654da123811a4fff
Christoph Berg <myon@debian.org>
2015-10-03 22:17:21 +0200

    Add non-sbuild chroots

--- jenkins/schroot-config.sh
+++ jenkins/schroot-config.sh
@@ -4,24 +4,32 @@
 
 # ./schroot-config.sh | sudo tee /etc/schroot/chroot.d/sbuild.conf
 
-set -eu
+set -e
 
 for dist in sid jessie wheezy squeeze utopic trusty precise; do
 	for arch in amd64 i386; do
-		cat <<-EOF
-		[$dist-pgdg-$arch-sbuild]
-		aliases=$dist-$arch
-		type=directory
-		groups=sbuild
-		root-groups=sbuild
-		source-groups=sbuild
-		source-root-groups=sbuild
-		directory=/home/chroot/$dist-$arch
-		union-type=overlay
-		union-overlay-directory=/var/run
-		profile=sbuild
-		EOF
-		[ $arch = i386 ] && echo "personality=linux32"
+		body="$(cat <<-EOF
+			type=directory
+			groups=sbuild
+			root-groups=sbuild
+			source-groups=sbuild
+			source-root-groups=sbuild
+			directory=/home/chroot/$dist-$arch
+			union-type=overlay
+			union-overlay-directory=/var/run
+			EOF
+			[ $arch = i386 ] && echo "personality=linux32"
+			echo
+		)"
+
+		echo "[$dist-$arch]"
+		[ $arch = amd64 ] && echo "aliases=$dist"
+		echo "$body"
+		echo
+
+		echo "[$dist-pgdg-$arch-sbuild]"
+		echo "profile=sbuild"
+		echo "$body"
 		echo
 	done
 done
